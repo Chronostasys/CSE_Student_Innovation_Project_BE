2a0e67d6dd017f98399c1524b6bbbd2c0ec41981
Xieyuschen <xieyuschen@gmail.com>
2020-12-16 21:32:17 +0800

    Clear codes and Start to implement design

--- models/web_user.go
+++ models/web_user.go
@@ -1,7 +1,5 @@
 package models
 
-import "github.com/jinzhu/gorm"
-
 type AuthUser struct {
 	Email                string `gorm:"primary_key"`
 	Username             string
@@ -12,10 +10,4 @@ type AuthUser struct {
 	Register_ip          string
 	Last_login_ip        string
 	Role                 string
-}
-type User struct {
-	gorm.Model
-	sso_user_id         string
-	activated_timestamp int64
-	lsat_post_timestamp int64
 }
