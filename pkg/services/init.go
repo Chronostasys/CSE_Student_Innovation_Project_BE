7fa75b805f8456b133951428670907049e4f450c
Xieyuschen <xieyuschen@gmail.com>
2020-12-18 22:21:25 +0800

    Init database

--- pkg/services/init.go
+++ pkg/services/init.go
@@ -1,9 +1,38 @@
 package services
 
 import (
+	"fmt"
+	atmodels "github.com/Pivot-Studio/Authorization-Template/models"
+	"github.com/Pivot-Studio/Authorization-Template/pkg/util"
+	"github.com/Pivot-Studio/CSE_Student_Innovation_Project/models"
 	"github.com/jinzhu/gorm"
 )
 
 var db *gorm.DB
 
+func dsn(settings atmodels.DbSettings) string {
+	// https://stackoverflow.com/questions/45040319/unsupported-scan-storing-driver-value-type-uint8-into-type-time-time
+	// Add ?parseTime=true
+	return fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true&charset=utf8mb4,utf8", settings.Username, settings.Password, settings.Hostname, settings.Dbname)
+}
+func init(){
+	databaseInit()
+}
+func databaseInit() {
+	conf := util.ReadSettingsFromFile("Config.json")
+	settings := conf.DbSettings
+	connStr := dsn(settings)
 
+	var err1 error
+	db, err1 = gorm.Open("mysql", connStr)
+	util.CheckError(err1)
+
+	var blog models.Blog
+	var team models.Team
+	var user atmodels.AuthUser
+	var invite models.Invite
+	var temp []interface{}
+
+	temp = append(temp,&blog,&team,&invite,&user)
+	util.CreateTableIfNotExist(db, temp)
+}
