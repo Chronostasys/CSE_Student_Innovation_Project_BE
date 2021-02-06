eaec7844915a121810889181dbb54652dcbc8d18
Xieyuschen <Xieyuschen@users.noreply.github.com>
2021-02-06 21:52:48 +0800

    Adjust get\

--- pkg/router/blog.go
+++ pkg/router/blog.go
@@ -68,23 +68,8 @@ func DeleteBlog(context *gin.Context){
 }
 
 func GetBlogs(context *gin.Context){
-	//team_id,_:=strconv.Atoi(context.PostForm("team_id"))
-	page,_:=strconv.Atoi(context.PostForm("page"))
-	list_size,_:=strconv.Atoi( context.PostForm("list_size"))
-	//is_descend,_:=strconv.ParseBool(context.PostForm("is_descend"))
-	//teamMember:=services.GetTeamMemberFromEmail(auth_email)
-	//if teamMember.Team_Id!=uint(team_id){
-	//	context.JSON(http.StatusBadRequest,gin.H{
-	//		"msg":"不在队伍中，无法查看",
-	//	})
-	//	return
-	//}
-	//if start_id<0||list_size<0{
-	//	context.JSON(http.StatusBadRequest, gin.H{
-	//		"msg": "不合规的id或表长",
-	//	})
-	//	return
-	//}
+	page,_:=strconv.Atoi(context.Param("page"))
+	list_size,_:=strconv.Atoi( context.Param("blog_num"))
 	blogs:=services.GetBlogs(page,list_size,true)
 	results:=[]map[string]interface{}{}
 	for _,temp:=range blogs{
